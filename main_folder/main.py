9581c1e91d450d229ce182c8fb2f7c0883a4a3f1
MuhammadMouzzam <mouzzamalimughal2005@gmail.com>
2024-07-28 19:26:09 +0500

    fifth commit

--- main_folder/main.py
+++ main_folder/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import blogs, users, auth, comments, votes
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title='Twitter (Clone)',summary='This is my 1st ever project using FastApi', description="Here, you can post Blogs, add comments, and also like any blog if you find it interesting. This is a CRUD based application meaning you can create, delete, update and read blogs.")
+
 
 @app.get("/")
 def main_page():
